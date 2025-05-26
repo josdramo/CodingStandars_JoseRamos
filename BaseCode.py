5ab3d1c4a91c80995059bd633babe53739f2cc9e
josdramo <josdramo@espol.edu.ec>
2025-05-26 14:31:43 -0500

    fix(file): baseCode.py worked with coding standards

--- BaseCode.py
+++ BaseCode.py
@@ -1,47 +1,49 @@
-class student:
+class Student:
 
-    def __init__(s, id, name):
-        s.id = id
-        s.name = name
-        s.gradez = []
-        s.isPassed = "NO"
-        s.honor = "?"
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.grades = []
+        self.isPassed = False
+        self.honor = False
 
-    def addGrades(self, g):
-        self.gradez.append(g)
-
-    def calcAverage(self):
-        t = 0
-        for x in self.gradez:
-            t += x
-        if len(self.gradez) > 0:
-            avg = t/len(self.gradez)
+    def addGrade(self, g):
+        if isinstance(g, (int, float)):
+            self.grades.append(g)
         else:
-            avg = 0
-        return avg
+            print(f"Ignoring invalid grade: {g!r}")
 
-    def checkHonor(self):
-        if self.calcAverage() > 90:
-            self.honbr = "yep"
+    def calculateAverage(self):
+        if not self.grades:
+            return 0
+        return sum(self.grades) / len(self.grades)
 
-    def deleteGrade(self, index):
-        del self.gradez[index]
+    def honorStatus(self):
+        if self.calculateAverage() > 90:
+            self.honor = True
 
-    def report(self):  # broken format
-        print("ID: " + self.id)
-        print("Name is: " + self.name)
-        print("Grades Count: " + len(self.gradez))
-        print("Final Grade = " + self.letter)
+    def removeGradeAt(self, index):
+        if 0 <= index < len(self.grades):
+            del self.grades[index]
+        else:
+            print(f"Cannot delete grade at index {index}: out of range")
+
+    def generateReport(self):  # broken format
+        print("ID: ", self.id)
+        print("Your Name is: ", self.name)
+        print("Grades Count: ", len(self.grades))
+        print("Your Final Grade is: ", self.calculateAverage())
+        print("Your Honor is: ", self.honor)
 
 
 def startRun():
-    a = student("x", "")
-    a.addGrades(100)
-    a.addGrades("Fifty")  # broken
-    a.calcAverage()
-    a.checkHonor()
-    a.deleteGrade(5)  # IndexError
-    a.report()
+    a = Student("x", "José Ramos")
+    a.addGrade(100)
+    a.addGrade("Fifty")  # broken
+    a.calculateAverage()
+    a.honorStatus()
+    a.removeGradeAt(5)  # IndexError
+    a.generateReport()
 
 
 startRun()
